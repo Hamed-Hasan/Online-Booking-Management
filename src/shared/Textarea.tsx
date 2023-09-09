d8c0424463186ca9a9e4782c4a597c7b418b96df
John Doe <hamedhasan422421@gmail.com>
2023-09-09 16:55:54 +0300

    🛠️ Refactor Textarea component ✨ Improved code structure and styling 📝 Added missing comments 🔧 Fixed eslint-disable-next-line 🐛 Resolved minor issues

---
+++ src/shared/Textarea.tsx
@@ -0,0 +1,22 @@
+import React, { TextareaHTMLAttributes } from "react";
+
+export interface TextareaProps
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+// eslint-disable-next-line react/display-name
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className = "", children, ...args }, ref) => {
+    return (
+      <textarea
+        ref={ref}
+        className={`block w-full text-sm rounded-2xl border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900 ${className}`}
+        rows={4}
+        {...args}
+      >
+        {children}
+      </textarea>
+    );
+  }
+);
+
+export default Textarea;
