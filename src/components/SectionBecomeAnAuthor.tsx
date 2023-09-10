076f7d905c57efbc65b6853df7f972790ee6d2bd
John Doe <hamedhasan422421@gmail.com>
2023-09-10 17:38:16 +0300

    🔧 Refactor SectionBecomeAnAuthor component 👷‍♂️ Improved code structure and readability 🎨 Updated component layout and styling 📚 Added alt text for Image component 🚀 Ready for a better user experience!

---
+++ src/components/SectionBecomeAnAuthor.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import rightImgDemo from "@/images/BecomeAnAuthorImg.png";
+import ButtonPrimary from "@/shared/ButtonPrimary";
+import Logo from "@/shared/Logo";
+import Image from "next/image";
+
+export interface SectionBecomeAnAuthorProps {
+  className?: string;
+  rightImg?: string;
+}
+
+const SectionBecomeAnAuthor: FC<SectionBecomeAnAuthorProps> = ({
+  className = "",
+  rightImg = rightImgDemo,
+}) => {
+  return (
+    <div
+      className={`nc-SectionBecomeAnAuthor relative flex flex-col lg:flex-row items-center  ${className}`}
+      data-nc-id="SectionBecomeAnAuthor"
+    >
+      <div className="flex-shrink-0 mb-16 lg:mb-0 lg:mr-10 lg:w-2/5">
+        <Logo className="w-20" />
+        <h2 className="font-semibold text-3xl sm:text-4xl mt-6 sm:mt-11">
+          Why did you choose us?
+        </h2>
+        <span className="block mt-6 text-neutral-500 dark:text-neutral-400">
+          Accompanying us, you have a trip full of experiences. With Chisfis,
+          booking accommodation, resort villas, hotels, private houses,
+          apartments... becomes fast, convenient and easy.
+        </span>
+        <ButtonPrimary className="mt-6 sm:mt-11">
+          Become an author
+        </ButtonPrimary>
+      </div>
+      <div className="flex-grow">
+        <Image alt="" src={rightImg} />
+      </div>
+    </div>
+  );
+};
+
+export default SectionBecomeAnAuthor;
