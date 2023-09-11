f4eb6da95c07bed2d034f9c7b5698122c6d1acd8
John Doe <hamedhasan422421@gmail.com>
2023-09-11 17:42:25 +0300

    🌟 Added Poppins font import 🎨 Updated SiteHeader component 📦 Imported ClientCommons module 🎉 Applied global styles from globals.css 🔧 Updated line-awesome font import 🌈 Styled index.scss 💄 Updated rc-slider styles 👣 Implemented Footer component 🔗 Added FooterNav component

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import { Poppins } from "next/font/google";
+import SiteHeader from "./(client-components)/(Header)/SiteHeader";
+import ClientCommons from "./ClientCommons";
+import "./globals.css";
+import "@/fonts/line-awesome-1.3.0/css/line-awesome.css";
+import "@/styles/index.scss";
+import "rc-slider/assets/index.css";
+import Footer from "@/components/Footer";
+import FooterNav from "@/components/FooterNav";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: any;
+}) {
+  return (
+    <html lang="en" className={poppins.className}>
+      <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+        <ClientCommons />
+        <SiteHeader />
+        {children}
+        <FooterNav />
+        <Footer />
+      </body>
+    </html>
+  );
+}
