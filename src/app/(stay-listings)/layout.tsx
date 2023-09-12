de0dad5d958aa07e288399f16f74d13ad3ea37ab
John Doe <hamedhasan422421@gmail.com>
2023-09-12 18:01:01 +0300

    Add a new feature to Layout. - Allow users to subscribe to the newsletter. - Add a button to the component that allows users to subscribe to the newsletter. - Update the styling of the component to accommodate the new button. - Add tests for the new feature.

---
+++ src/app/(stay-listings)/layout.tsx
@@ -0,0 +1,47 @@
+import BackgroundSection from "@/components/BackgroundSection";
+import BgGlassmorphism from "@/components/BgGlassmorphism";
+import SectionGridAuthorBox from "@/components/SectionGridAuthorBox";
+import SectionSliderNewCategories from "@/components/SectionSliderNewCategories";
+import SectionSubscribe2 from "@/components/SectionSubscribe2";
+import React, { ReactNode } from "react";
+import SectionHeroArchivePage from "../(server-components)/SectionHeroArchivePage";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className={`nc-ListingStayPage relative `}>
+      <BgGlassmorphism />
+
+      {/* SECTION HERO */}
+      <div className="container pt-10 pb-24 lg:pt-16 lg:pb-28">
+        <SectionHeroArchivePage currentPage="Stays" currentTab="Stays" />
+      </div>
+
+      {children}
+
+      <div className="container overflow-hidden">
+        {/* SECTION 1 */}
+        <div className="relative py-16">
+          <BackgroundSection />
+          <SectionSliderNewCategories
+            heading="Explore by types of stays"
+            subHeading="Explore houses based on 10 types of stays"
+            categoryCardType="card5"
+            itemPerRow={5}
+            sliderStyle="style2"
+          />
+        </div>
+
+        {/* SECTION */}
+        <SectionSubscribe2 className="py-24 lg:py-28" />
+
+        {/* SECTION */}
+        <div className="relative py-16 mb-24 lg:mb-28">
+          <BackgroundSection className="bg-orange-50 dark:bg-black dark:bg-opacity-20 " />
+          <SectionGridAuthorBox />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
