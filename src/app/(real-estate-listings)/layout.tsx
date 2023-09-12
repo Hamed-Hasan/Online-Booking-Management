abf305533dd86129c9363fe51dce0439904dd87a
John Doe <hamedhasan422421@gmail.com>
2023-09-12 19:38:20 +0300

    🚀 Implemented new features in Layout - Added dynamic content rendering - Integrated Redux for state management - Enhanced user experience

---
+++ src/app/(real-estate-listings)/layout.tsx
@@ -0,0 +1,43 @@
+import BackgroundSection from "@/components/BackgroundSection";
+import SectionGridAuthorBox from "@/components/SectionGridAuthorBox";
+import SectionSliderNewCategories from "@/components/SectionSliderNewCategories";
+import SectionSubscribe2 from "@/components/SectionSubscribe2";
+import React, { ReactNode } from "react";
+import SectionHero2ArchivePage from "../(server-components)/SectionHero2ArchivePage";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="nc-ListingRealEstateMapPage">
+      <div className="container pb-24 lg:pb-28">
+        <SectionHero2ArchivePage />
+      </div>
+
+      {children}
+
+      <div className="container overflow-hidden">
+        {/* SECTION 1 */}
+        <div className="relative py-16">
+          <BackgroundSection />
+          <SectionSliderNewCategories
+            heading="Explore by types of stays"
+            subHeading="Explore houses based on 10 types of stays"
+            categoryCardType="card5"
+            itemPerRow={5}
+            sliderStyle="style2"
+          />
+        </div>
+
+        {/* SECTION */}
+        <SectionSubscribe2 className="py-24 lg:py-28" />
+
+        {/* SECTION */}
+        <div className="relative py-16 mb-24 lg:mb-28">
+          <BackgroundSection className="bg-orange-50 dark:bg-black dark:bg-opacity-20 " />
+          <SectionGridAuthorBox />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
