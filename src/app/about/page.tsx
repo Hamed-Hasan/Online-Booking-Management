3644bb36446e01485ca710bd46a33ea17d044eaf
John Doe <hamedhasan422421@gmail.com>
2023-09-12 17:51:09 +0300

    👋 Update about page with hero section :sparkles:
    Added a hero section to the about page.
    
    The hero section includes an image, heading, and subheading.
    
    The image is a photo of the company's founders.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,41 @@
+import rightImg from "@/images/about-hero-right.png";
+import React, { FC } from "react";
+import SectionFounder from "./SectionFounder";
+import SectionStatistic from "./SectionStatistic";
+import SectionHero from "./SectionHero";
+import BgGlassmorphism from "@/components/BgGlassmorphism";
+import BackgroundSection from "@/components/BackgroundSection";
+import SectionClientSay from "@/components/SectionClientSay";
+import SectionSubscribe2 from "@/components/SectionSubscribe2";
+
+export interface PageAboutProps {}
+
+const PageAbout: FC<PageAboutProps> = ({}) => {
+  return (
+    <div className={`nc-PageAbout overflow-hidden relative`}>
+      {/* ======== BG GLASS ======== */}
+      <BgGlassmorphism />
+
+      <div className="container py-16 lg:py-28 space-y-16 lg:space-y-28">
+        <SectionHero
+          rightImg={rightImg}
+          heading="👋 About Us."
+          btnText=""
+          subHeading="We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain millions of people in the around the world."
+        />
+
+        <SectionFounder />
+        <div className="relative py-16">
+          <BackgroundSection />
+          <SectionClientSay />
+        </div>
+
+        <SectionStatistic />
+
+        <SectionSubscribe2 />
+      </div>
+    </div>
+  );
+};
+
+export default PageAbout;
