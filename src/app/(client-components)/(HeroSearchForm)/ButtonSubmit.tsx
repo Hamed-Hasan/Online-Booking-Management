f195a8f1b3899fede287c09f87768ab2a675f3cd
John Doe <hamedhasan422421@gmail.com>
2023-09-13 19:37:25 +0300

    🚀 Added ButtonSubmit component 💥 Improved button styling 📝 Updated ButtonSubmit component 🐛 Fixed issue in ButtonSubmit ✨ Added hover effect to ButtonSubmit 🔧 Refactored ButtonSubmit code 🌟 Enhanced ButtonSubmit functionality

---
+++ src/app/(client-components)/(HeroSearchForm)/ButtonSubmit.tsx
@@ -0,0 +1,35 @@
+import { PathName } from "@/routers/types";
+import Link from "next/link";
+import React, { FC } from "react";
+
+interface Props {
+  href?: PathName;
+}
+
+const ButtonSubmit: FC<Props> = ({ href = "/listing-stay-map" }) => {
+  return (
+    <Link
+      href={href}
+      type="button"
+      className="h-14 md:h-16 w-full md:w-16 rounded-full bg-primary-6000 hover:bg-primary-700 flex items-center justify-center text-neutral-50 focus:outline-none"
+    >
+      <span className="mr-3 md:hidden">Search</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </Link>
+  );
+};
+
+export default ButtonSubmit;
