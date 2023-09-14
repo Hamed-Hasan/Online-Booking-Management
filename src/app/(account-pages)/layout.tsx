60f384f1afa2b0469c9e34306072287aeb88f57b
John Doe <hamedhasan422421@gmail.com>
2023-09-14 17:08:53 +0300

    🎉 Refactor the CommonLayout component 🚧 Improve code readability and structure 🌟 Add missing comments for clarity 🐛 Fix minor styling issues in CommonLayout 📚 Update documentation for CommonLayout 🏗️ Optimize imports in the CommonLayout component 🔄 Reformat code to follow coding conventions

---
+++ src/app/(account-pages)/layout.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { Nav } from "./(components)/Nav";
+
+export interface CommonLayoutProps {
+  children?: React.ReactNode;
+}
+
+const CommonLayout: FC<CommonLayoutProps> = ({ children }) => {
+  return (
+    <div className="nc-CommonLayoutAccount bg-neutral-50 dark:bg-neutral-900">
+      <div className="border-b border-neutral-200 dark:border-neutral-700 pt-12 bg-white dark:bg-neutral-800">
+        <Nav />
+      </div>
+      <div className="container pt-14 sm:pt-20 pb-24 lg:pb-32">{children}</div>
+    </div>
+  );
+};
+
+export default CommonLayout;
